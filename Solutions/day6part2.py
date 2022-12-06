074c1e8818b9724bd5f46fe9af2b3fbd0f551f4f
Charlotte <charlotte.gayton@gmail.com>
2022-12-06 10:08:43 +0000

    Day6

---
+++ Solutions/day6part2.py
@@ -0,0 +1,22 @@
+input_file = open("Inputs/day6.txt", "r")
+data = input_file.read()
+data_list = list(data)
+
+def duplicate_checker(check_numbers):
+    remove_duplicates = list(dict.fromkeys(check_numbers))
+    if check_numbers == remove_duplicates: 
+        return True
+    else: 
+        return False
+
+
+check_numbers = []
+counter = 0
+for each in data_list:
+    check_numbers.append(each)
+    counter += 1
+    if len(check_numbers) == 14:     
+        if duplicate_checker(check_numbers) == False:
+            check_numbers = check_numbers[1:]
+        else:
+            print(counter)
